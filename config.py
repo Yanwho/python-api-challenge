258b21f5d0ad71642ea4febe797dcfe9e9471ea6
yanwho <yanwho@yahoo.com>
2021-01-18 21:39:02 +0000

    moved to gcp jupyter for better plotting functioanlity and completed heat map.  Had to modify some file paths following platform change

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 api_key = "a2c2be2564985016236ba3b97bb26e4d"
 
 google_key = "AIzaSyBDmjIX3uuuU7JZVTmJEAyv1qaC38D7RtI"
+
+mapbox_token = "pk.eyJ1IjoieWFud2hvIiwiYSI6ImNrazJ4eWFmMzE1cjIydm55c2NtbnExMjgifQ.6_sx9gMM5jQY0lHqnGVIEg"
